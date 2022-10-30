862ae940052a692c8d1fa7ef8d70921ee0b2b41f
mirkomimic <mirkomimic133@gmail.com>
2022-10-30 14:50:20 +0100

    shop

--- index.php
+++ index.php
@@ -3,32 +3,36 @@ session_start();
 require "DB/users.php";
 require "DB/items.php";
 
+
+// login
+if (isset($_POST['login'])) {
+  if (isset($_POST['username']) && isset($_POST['password'])) {
+    if (login($_POST['username'], $_POST['password'])) {
+      $_SESSION['user'] = $_POST['username'];
+      include "shop.php";
+      exit();
+    }
+  }
+}
+
 if (!isset($_SESSION['user'])) {
   include "login.php";
 } else {
   include "shop.php";
 }
 
-// login
-if (isset($_POST['login'])) {
-  echo "mirko";
-  if (login($_POST['username'], $_POST['password'])) {
-    header("Location: .");
-    include "shop.php";
-    exit();
-  };
-}
 
 // login function
 function login($username, $password) {
   global $users;
+
   foreach ($users as $user) {
     if ($user['username'] == $username && $user['password'] == $password) {
-      $_SESSION['user'] = $username;
-    } elseif ($user['username'] == "admin" && $user['password'] == "admin"){
-      $_SESSION['admin'] = $username;
-    }
-  }
+      return true;
+    } 
+  } 
+  return false;
+  
 }
 
 ?>
--- shop.php
+++ shop.php
@@ -10,6 +10,38 @@
 <body>
   <div class="container">
     <h1>SHOP</h1>
+    <div class="logout">
+      <p>Welcome: <?php echo $_SESSION['user']; ?></p>
+      <form action="logout.php" method="get">
+        <input type="submit" value="Logout" name="logout">
+      </form>
+    </div>
+    <div class="shop-container">
+      <aside></aside>
+
+      <section class="shop-cards">
+        <?php foreach($_SESSION['items'] as $item) : ?>
+        <form action="" method="get">
+          <article class="shop-card">
+            <input type="hidden" name="id" value="<?= $item['id'] ?>">
+            <div class="shop-card-img">
+              <img src="https://picsum.photos/250/170" alt="slika">
+            </div>
+            <h3><?= $item['name'] ?></h3>
+            <p class="cena">Price: <?= $item['price'] ?></p>
+            <input type="submit" name="order" value="Order">
+            <div class="admin-kolicina">
+              <p>Stock: <?= $item['stock'] ?></p>
+            </div>
+            <div class="admin-shop-input">
+              <input type="submit" value="Add" name="add-item">
+              <input type="submit" value="Edit" name="edit-item">
+            </div>
+         </article>
+        </form>
+        <?php endforeach ; ?>
+      </section>
+    </div>
   </div>
 </body>
 </html>
